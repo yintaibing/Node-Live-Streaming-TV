5689533fe3c5d52ab07cfc507a5df4e99140ac8d
yintaibing <yintaibing@foreveross.com>
2016-04-19 15:03:00 +0800

    add a 'myutil'

---
+++ server/myutil/myutil.js
@@ -0,0 +1,36 @@
+/*
+ * @File:   myutil.js
+ * @Author: yintaibing, SCUT Software Engineering, Grade 2012, Class 6.
+ * @Date:   2016/04/19
+ */
+
+var crypto = require('crypto');
+var qs = require('querystring');
+
+var KEY = 'TurtleTV';
+exports.desEn = function(str) {
+	var cipher = crypto.createCipher('des', KEY);
+	var en = cipher.update(str, 'utf8', 'hex');
+	en += cipher.final('hex');
+	return en;
+};
+
+exports.desDe = function(str) {
+	var decipher = crypto.createDecipher('des', KEY);
+	var de = decipher.update(str, 'hex', 'utf8');
+	de += decipher.final('utf8');
+	return de;
+};
+
+exports.parseQueryString = function(str) {
+	return qs.parse(str);
+};
+
+exports.removeFromAry = function(ary, target) {
+	var length = ary.length;
+	for (var i = 0; i < length; i++) {
+		if (ary[i] === target) {
+			return ary.slice(0, i).concat(ary.slice(i + 1));
+		}
+	}
+};
